8eae060af57ff93f735889f90d3db40fcad37988
Pooja D <poojapreethi2001@gmail.com>
2025-07-16 11:26:35 +0530

    Update WelcomeScreen.js

--- client/src/components/WelcomeScreen/WelcomeScreen.js
+++ client/src/components/WelcomeScreen/WelcomeScreen.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import backgroundImage from './Assests/background-image.png';
 import './WelcomeScreen.css';
 
 const WelcomeScreen = () => {
   return (
     <div className="welcome-screen">
-      <img src="/welcome-image.svg" alt="Welcome" className="empty-state" /> {/* Replace with correct image path */}
+      <img src={backgroundImage} alt="Pocket Notes Background" className="welcome-img" />
       <h2>Pocket Notes</h2>
       <p>Send and receive messages without keeping your phone online.</p>
       <p>Use Pocket Notes on up to 4 linked devices and 1 mobile phone.</p>
