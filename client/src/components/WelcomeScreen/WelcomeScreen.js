bdaea9e936cffe5eae0baf456626a16eba37757d
Pooja D <poojapreethi2001@gmail.com>
2025-07-18 21:16:10 +0530

    Update WelcomeScreen.js

--- client/src/components/WelcomeScreen/WelcomeScreen.js
+++ client/src/components/WelcomeScreen/WelcomeScreen.js
@@ -4,10 +4,10 @@ import styles from './WelcomeScreen.module.css';
 
 const WelcomeScreen = () => {
   return (
-    <div className={styles.welcome-section}>
-    <div className={styles.welcome-screen}>
+    <div className={styles.welcomesection}>
+    <div className={styles.welcomeScreen}>
       <img src={backgroundImage} alt="Pocket Notes Background" />
-     <div className={styles.empty-state}>
+     <div className={styles.emptyState}>
       <h2>Pocket Notes</h2>
       <p>Send and receive messages without keeping your phone online.</p>
       <p>Use Pocket Notes on up to 4 linked devices and 1 mobile phone.</p>
