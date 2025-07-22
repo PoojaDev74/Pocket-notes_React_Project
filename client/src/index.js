e7076760935ca8209414c48c66d9b8d74bb50aae
Pooja D <poojapreethi2001@gmail.com>
2025-07-22 20:08:10 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,7 @@ import NotesProvider from "./Context/NotesContext";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+ <React.StrictMode>
    <BrowserRouter> 
       <NotesProvider>
         <App />
