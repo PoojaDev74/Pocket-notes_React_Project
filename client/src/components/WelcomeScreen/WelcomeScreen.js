87fc7ae1bd313b35fae0946955735e8e9e29811a
Pooja D <poojapreethi2001@gmail.com>
2025-07-23 15:51:15 +0530

    Update WelcomeScreen.js

--- client/src/components/WelcomeScreen/WelcomeScreen.js
+++ client/src/components/WelcomeScreen/WelcomeScreen.js
@@ -11,8 +11,8 @@ const WelcomeScreen = () => {
            <h2>Pocket Notes</h2>
             <p>Send and receive messages without keeping your phone online.</p>
             <p>Use Pocket Notes on up to 4 linked devices and 1 mobile phone.</p>
-         <div className={styles.footer}>🔒 end-to-end encrypted</div>
         </div>
+            <div className={styles.footer}>🔒 end-to-end encrypted</div>
       </div>
     </div>
   );
